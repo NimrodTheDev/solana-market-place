d231448720ae821facd78a2a0106a1710d59e647
dev.pete.py <85735876+phanstudio@users.noreply.github.com>
2025-05-10 19:40:05 +0100

    Update models.py

--- backend/systems/models.py
+++ backend/systems/models.py
@@ -63,7 +63,7 @@ class Coin(models.Model): # add a way to make things more strick
     creator = models.ForeignKey(SolanaUser, on_delete=models.CASCADE, related_name='coins', to_field="wallet_address")
     created_at = models.DateTimeField(auto_now_add=True)
     total_supply = models.DecimalField(max_digits=20, decimal_places=8)
-    image_url = models.URLField(max_length=500, unique=True)
+    image_url = models.URLField(max_length=500)
     ticker = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     telegram = models.CharField(max_length=255, blank=True, null=True)
