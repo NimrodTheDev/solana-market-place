ab7f7222b2b2b2b602b5a49044d1c108038756b9
dev.pete.py <85735876+phanstudio@users.noreply.github.com>
2025-05-30 15:17:19 +0100

    Update permissions.py

--- backend/systems/permissions.py
+++ backend/systems/permissions.py
@@ -1,12 +1,9 @@
 from rest_framework.permissions import BasePermission
 from dotenv import load_dotenv
 import os
-import logging
 load_dotenv()
-logger = logging.getLogger(__name__)
 class IsCronjobRequest(BasePermission):
     def has_permission(self, request, view):
         cron_key_env = os.getenv('CRON_KEY')
         cron_key_req = request.META.get("HTTP_X_CRON_SECRET")
         return cron_key_req == cron_key_env
-
