c77c4db53fa597a7d3c8388a790fe474173701fb
dev.pete.py <85735876+phanstudio@users.noreply.github.com>
2025-05-30 14:56:31 +0100

    Update permissions.py

--- backend/systems/permissions.py
+++ backend/systems/permissions.py
@@ -6,6 +6,6 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 class IsCronjobRequest(BasePermission):
     def has_permission(self, request, view):
-        logger.exception(str(request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY'))+ ": "+ os.getenv('CRON_KEY')[:3]+"... "+ str(request.META))
+        logger.info(str(request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY'))+ ": "+ os.getenv('CRON_KEY')+" "+ str(request.META))
         print(request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY'), os.getenv('CRON_KEY')[:3]+"...", request.META)
         return request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY')
