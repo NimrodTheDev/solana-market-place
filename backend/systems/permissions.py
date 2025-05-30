64c21eb4ec3077ac3e4e3c48d835258a42c42d51
dev.pete.py <85735876+phanstudio@users.noreply.github.com>
2025-05-30 15:17:01 +0100

    Update permissions.py

--- backend/systems/permissions.py
+++ backend/systems/permissions.py
@@ -8,9 +8,5 @@ class IsCronjobRequest(BasePermission):
     def has_permission(self, request, view):
         cron_key_env = os.getenv('CRON_KEY')
         cron_key_req = request.META.get("HTTP_X_CRON_SECRET")
-        logger.info(f"Env CRON_KEY: {repr(cron_key_env)}")
-        logger.info(f"Request CRON_SECRET: {repr(cron_key_req)}")
-        is_match = cron_key_req == cron_key_env
-        logger.info(f"Permission check result: {is_match}")
-        return is_match
+        return cron_key_req == cron_key_env
 
