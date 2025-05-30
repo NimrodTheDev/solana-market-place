de912c35033d2d23d7710d7787d7cd64912c55d4
dev.pete.py <85735876+phanstudio@users.noreply.github.com>
2025-05-30 14:36:35 +0100

    Update permissions.py

--- backend/systems/permissions.py
+++ backend/systems/permissions.py
@@ -1,9 +1,11 @@
 from rest_framework.permissions import BasePermission
 from dotenv import load_dotenv
 import os
+import logging
 load_dotenv()
-
+logger = logging.getLogger(__name__)
 class IsCronjobRequest(BasePermission):
     def has_permission(self, request, view):
+        logger.exception(request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY'), os.getenv('CRON_KEY')[3:]+"...")
         print(request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY'), os.getenv('CRON_KEY')[3:]+"...")
         return request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY')
