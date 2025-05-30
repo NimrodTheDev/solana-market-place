a61134f731385903fac9581c947bc9c44eb4eaa3
dev.pete.py <85735876+phanstudio@users.noreply.github.com>
2025-05-30 14:23:08 +0100

    Update permissions.py

--- backend/systems/permissions.py
+++ backend/systems/permissions.py
@@ -5,4 +5,5 @@ load_dotenv()
 
 class IsCronjobRequest(BasePermission):
     def has_permission(self, request, view):
+        print(request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY'), os.getenv('CRON_KEY')[3:]+"...")
         return request.META.get("HTTP_X_CRON_SECRET") == os.getenv('CRON_KEY')
